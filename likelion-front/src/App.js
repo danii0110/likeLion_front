bf377fd7dc4f0dffadc7a468506ea325bb943271
danii <danii0110@naver.com>
2023-05-13 03:04:00 +0900

    react-router-dom
    1. cd likelion-front
    2. npm install react-router-dom

--- likelion-front/src/App.js
+++ likelion-front/src/App.js
@@ -1,10 +1,28 @@
+import React, {Component} from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+
+import Header from './Header';
+import Main from './Main';
+import NotFound from './NotFound';
+
 import Login from "./Login";
 import Register from "./Register";
 
+
 export default function App() {
   return (
-    <div>
-      <Register/>
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />}></Route>
+
+
+          {/* 상단에 위치한 라우트들의 규칙을 모드 확인, 일치하는 라우트가 없는 경우 처리 */}
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+
+      </BrowserRouter>
     </div>
   );
-}
+};
