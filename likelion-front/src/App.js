7bb83158431283fe4a52e7fc86ef113e37396260
danii <danii0110@naver.com>
2023-05-15 20:18:17 +0900

    .

--- likelion-front/src/App.js
+++ likelion-front/src/App.js
@@ -6,27 +6,34 @@ import Header from './Header';
 import Main from './Main';
 import NotFound from './NotFound';
 
-import Users from './User';
-
 import Login from "./Login";
 import Register from "./Register";
 
 
 export default function App() {
   return (
-    <div className='App'>
-      <BrowserRouter>
-        <Routes>
-          {/* <Route path="/" element={<Main />}></Route> */}
+    <Login/>
+    // <Main>
+    //   <Routes>
+    //     <Route path=''></Route>
+    //   </Routes>
+    // </Main>
+    // <Routes>
+    //   <Route path="/login" element={<Login/>}></Route>
+    // </Routes>
+    // <div className='App'>
+    //   <BrowserRouter>
+    //     <Routes>
+    //       {/* <Route path="/" element={<Main />}></Route> */}
 
-          <Users/>
+    //       <Users/>
 
 
-          {/* 상단에 위치한 라우트들의 규칙을 모드 확인, 일치하는 라우트가 없는 경우 처리 */}
-          <Route path="*" element={<NotFound />}></Route>
-        </Routes>
+    //       {/* 상단에 위치한 라우트들의 규칙을 모드 확인, 일치하는 라우트가 없는 경우 처리 */}
+    //       <Route path="*" element={<NotFound />}></Route>
+    //     </Routes>
 
-      </BrowserRouter>
-    </div>
+    //   </BrowserRouter>
+    // </div>
   );
 };
---
+++ likelion-front/src/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Oopsla from "./assets/Image/Logo.png"
+
+export default function Header() {
+
+    return (
+        <img className="oopsla-logo" src={Oopsla} alt="oops!a logo" title="천다인이 로고 만듦"/>
+    );
+}
